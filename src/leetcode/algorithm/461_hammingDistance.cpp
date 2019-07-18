b5060759d782e390c31b73a0bcb42b790bedbe3e
PotterXu <xujingchuan1995@gmail.com>
2019-07-18 21:12:26 +0800

    [Leetcode] Refatoring of hammingDistance.cpp

--- src/leetcode/algorithm/461_hammingDistance.cpp
+++ src/leetcode/algorithm/461_hammingDistance.cpp
@@ -2,7 +2,7 @@ int hammingDistance(int x, int y) {
     int rv = 0;
     x ^= y;
     while (x != 0) {
-        if (x & 1 != 0) {
+        if ((x & 1) != 0) {
             rv++;
         }
         x /= 2;
