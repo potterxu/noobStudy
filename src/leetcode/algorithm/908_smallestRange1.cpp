46f4f5e0b1cf7761f0d7b19d1901969afb62d963
Potter XU <xujingchuan1995@gmail.com>
2019-07-19 15:17:19 +0800

    [Leetcode] algorithm: make 908_smallestRange1 compatiable to windows.

--- src/leetcode/algorithm/908_smallestRange1.cpp
+++ src/leetcode/algorithm/908_smallestRange1.cpp
@@ -4,7 +4,9 @@
 using namespace std;
 
 int smallestRangeI(vector<int>& A, int K) {
-    auto [min, max] = minmax_element(A.begin(), A.end());
-    int rv = *max - *min - K - K;
+    //auto [min, max] = minmax_element(A.begin(), A.end());
+    //int rv = *max - *min - K - K;
+    sort(A.begin(), A.end());
+    int rv = A[A.size() - 1] - A[0] - K - K;
     return rv > 0 ? rv : 0;
 }
