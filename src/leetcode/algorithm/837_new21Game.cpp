57be3c7733988ed24db77d305f30354db7c884a3
PotterXu <xujingchuan1995@gmail.com>
2020-01-03 00:21:33 +0800

    [Leetcode] Add solution for some problems.
    837_new21Game.cpp

---
+++ src/leetcode/algorithm/837_new21Game.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+double new21Game(int N, int K, int W) {
+    if (N-K >= W) {
+        return 1;
+    }
+    auto m = vector<double>(K+W, 0.0); // The probability to win if we already get x points
+    for (int i = K; i <= N; ++i) {
+        m[i] = 1.0;
+    }
+    double s = N-K+1;
+    for (int i = K - 1; i >= 0; --i) {
+        m[i] = s/W;
+        s += m[i] - m[i+W];
+    }
+    return m[0];
+}
